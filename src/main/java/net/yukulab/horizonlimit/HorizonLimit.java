365c7c74ee03c03bf620775df416fd33ecfc962b
turtton <top.gear7509@turtton.net>
2023-11-02 22:27:51 +0900

    :tada: Init commit

---
+++ src/main/java/net/yukulab/horizonlimit/HorizonLimit.java
@@ -0,0 +1,10 @@
+package net.yukulab.horizonlimit;
+
+import net.fabricmc.api.ModInitializer;
+
+public class HorizonLimit implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/net/yukulab/horizonlimit/client/HorizonLimitClient.java
@@ -0,0 +1,10 @@
+package net.yukulab.horizonlimit.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class HorizonLimitClient implements ClientModInitializer  {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
