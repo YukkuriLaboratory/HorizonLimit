7925e0bdd43b6c3e0245823938e465ee9ad9e902
turtton <top.gear7509@turtton.net>
2023-11-03 15:53:33 +0900

    :hammer: Implement limit checking

--- src/main/java/net/yukulab/horizonlimit/mixin/MixinServerPlayerEntity.java
+++ src/main/java/net/yukulab/horizonlimit/mixin/MixinServerPlayerEntity.java
@@ -1,6 +1,9 @@
 package net.yukulab.horizonlimit.mixin;
 
+import java.util.HashMap;
 import net.minecraft.server.network.ServerPlayerEntity;
+import net.yukulab.horizonlimit.HorizonLimitDamageSource;
+import net.yukulab.horizonlimit.network.packet.play.UpdateCountdownS2CPacket;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
@@ -10,14 +13,38 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(ServerPlayerEntity.class)
 public class MixinServerPlayerEntity {
     @Unique
-    private int horizonlimit$overTimeTick = -1;
+    private int horizonlimit$currentOverTimeTick = -1;
 
     @Inject(method = "tick", at = @At("RETURN"))
     private void checkYHeight(CallbackInfo ci) {
         var player = (ServerPlayerEntity) (Object) this;
+        var world = player.getServerWorld();
         var y = player.getY();
-        // TODO check player over limit
-
-        // TODO send countdown
+        // これがnullになることはない
+        var config = player.getServer().horizonlimit$getServerConfig();
+        var worldName = world.getDimensionKey().getRegistry().getNamespace();
+        var playerMap = config.limit()
+                .computeIfAbsent(worldName, (key) -> new HashMap<>());
+        var limit = playerMap.get(player.getUuid());
+        if (limit == null) {
+            return;
+        }
+        var limitTick = config.timeLimit();
+        var isSkySide = limit.isSkySide();
+        var limitHeight = limit.limit();
+        if (isSkySide && y > limitHeight || y < limitHeight) {
+            if (horizonlimit$currentOverTimeTick == -1) {
+                horizonlimit$currentOverTimeTick = limitTick;
+            }
+            if (horizonlimit$currentOverTimeTick > 0) {
+                horizonlimit$currentOverTimeTick--;
+            } else {
+                var source = new HorizonLimitDamageSource();
+                player.damage(source, player.getHealth() + 1);
+            }
+        } else {
+            horizonlimit$currentOverTimeTick = -1;
+        }
+        UpdateCountdownS2CPacket.send(player, horizonlimit$currentOverTimeTick);
     }
 }
