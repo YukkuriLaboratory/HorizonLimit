788b1f5dd132581718c2ad729ddd0bf74d8a2629
turtton <top.gear7509@turtton.net>
2023-11-03 12:15:17 +0900

    :hammer: Add environment annotation

--- src/main/java/net/yukulab/horizonlimit/client/HorizonLimitClient.java
+++ src/main/java/net/yukulab/horizonlimit/client/HorizonLimitClient.java
@@ -1,7 +1,10 @@
 package net.yukulab.horizonlimit.client;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
 
+@Environment(EnvType.CLIENT)
 public class HorizonLimitClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {}
