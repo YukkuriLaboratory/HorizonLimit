5521c3e51f2407fd04a67d1a3a1c8774cefeb8b7
turtton <top.gear7509@turtton.net>
2023-11-03 12:20:49 +0900

    :hammer: Add countdown packet

--- src/main/java/net/yukulab/horizonlimit/network/Networking.java
+++ src/main/java/net/yukulab/horizonlimit/network/Networking.java
@@ -3,14 +3,17 @@ package net.yukulab.horizonlimit.network;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.fabricmc.fabric.api.client.networking.v1.ClientLoginNetworking;
+import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
 import net.fabricmc.fabric.api.networking.v1.ServerLoginConnectionEvents;
 import net.fabricmc.fabric.api.networking.v1.ServerLoginNetworking;
 import net.minecraft.util.Identifier;
 import net.yukulab.horizonlimit.HorizonLimit;
 import net.yukulab.horizonlimit.network.packet.HandShakeS2CPacket;
+import net.yukulab.horizonlimit.network.packet.play.UpdateCountdownS2CPacket;
 
 public class Networking {
     public static final Identifier HANDSHAKE = id("handshake");
+    public static final Identifier UPDATE_COUNTDOWN = id("updatecountdown");
 
     public static void registerServerReceivers() {
         ServerLoginConnectionEvents.QUERY_START.register(HandShakeS2CPacket::sendQuery);
@@ -20,6 +23,7 @@ public class Networking {
     @Environment(EnvType.CLIENT)
     public static void registerClientReceivers() {
         ClientLoginNetworking.registerGlobalReceiver(HANDSHAKE, HandShakeS2CPacket::onHandShakeClient);
+        ClientPlayNetworking.registerGlobalReceiver(UPDATE_COUNTDOWN, UpdateCountdownS2CPacket::onReceive);
     }
 
 
---
+++ src/main/java/net/yukulab/horizonlimit/network/packet/play/UpdateCountdownS2CPacket.java
@@ -0,0 +1,31 @@
+package net.yukulab.horizonlimit.network.packet.play;
+
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.fabricmc.fabric.api.networking.v1.PacketByteBufs;
+import net.fabricmc.fabric.api.networking.v1.PacketSender;
+import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.network.ClientPlayNetworkHandler;
+import net.minecraft.network.PacketByteBuf;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.yukulab.horizonlimit.network.Networking;
+
+public class UpdateCountdownS2CPacket {
+    public static void send(ServerPlayerEntity player, int countdown) {
+        var buf = PacketByteBufs.create();
+        buf.writeInt(countdown);
+        ServerPlayNetworking.send(player, Networking.UPDATE_COUNTDOWN, buf);
+    }
+
+    @Environment(EnvType.CLIENT)
+    public static void onReceive(
+            MinecraftClient client,
+            ClientPlayNetworkHandler handler,
+            PacketByteBuf buf,
+            PacketSender responseSender
+    ) {
+        var currentCountdown = buf.readInt();
+        // TODO update point
+    }
+}
