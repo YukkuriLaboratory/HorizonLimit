47671bd5322e8fd49ab91e581c789adac4833e3b
turtton <top.gear7509@turtton.net>
2023-11-03 11:53:21 +0900

    :hammer: Add variables

--- src/main/java/net/yukulab/horizonlimit/HorizonLimit.java
+++ src/main/java/net/yukulab/horizonlimit/HorizonLimit.java
@@ -1,8 +1,14 @@
 package net.yukulab.horizonlimit;
 
 import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class HorizonLimit implements ModInitializer {
+    public static final String MOD_NAME = "HorizonLimit";
+    public static final String MOD_ID = MOD_NAME.toLowerCase();
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
     @Override
-    public void onInitialize() {}
+    public void onInitialize() {
+    }
 }
